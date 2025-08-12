63cd2be7d8072de21f9b8876cab719349cf678ac
ashi <ashi888032@gmail.com>
2025-08-12 22:14:04 +0530

    change in casing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "./components/header";
+import Header from "./components/Header";
+
 
 
 const geistSans = Geist({
