e242d82423b6111821116042d31772cd05594989
ashi <ashi888032@gmail.com>
2025-09-18 00:23:40 +0530

    minor fix

--- src/utils/cashfree.ts
+++ src/utils/cashfree.ts
@@ -6,7 +6,7 @@ import { load } from "@cashfreepayments/cashfree-js";
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 const initializeSDK = async function (): Promise<any> {
     const sdk = await load({
-        mode: process.env.NEXT_PUBLIC_CASHFREE_ENV === "production"
+        mode: process.env.CASHFREE_ENV === "production"
             ? "production"
             : "sandbox",
     });
